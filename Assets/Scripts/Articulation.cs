cad85987c321b3fb02218387ee0dc56f1cac62f0
Jean Lamarre <jean.lamarre.1@ens.etsmtl.ca>
2020-06-04 01:51:22 -0400

    First implentation

---
+++ Assets/Scripts/Articulation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Articulation : MonoBehaviour
+{
+    private Joint joint;
+    private Muscle muscle;
+    private Bone bone1;
+    private Bone bone2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = this.GetComponentInChildren<Joint>();
+        muscle = this.GetComponentInChildren<Muscle>();
+        bone1 = this.GetComponentInChildren<Bone>();
+        bone2 = this.GetComponentInChildren<Bone>();
+    }
+
+    void FixedUpdate()
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad == null)
+            return; // No gamepad connected.
+
+        if (gamepad.rightTrigger.isPressed)
+        {
+            muscle.contract();
+        }
+        else
+        {
+            muscle.relax();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Muscle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muscle : MonoBehaviour
+{
+    private float force;
+
+    private float lenght;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        lenght = this.transform.localScale.y;
+        force = lenght / 50;
+    }
+
+    public void contract()
+    {
+
+        if (this.transform.localScale.y > (lenght / 50))
+        {
+            this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y - force, this.transform.localScale.z);
+        }
+    }
+
+    public void relax()
+    {
+        if (this.transform.localScale.y > lenght)
+        {
+            this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y - force, this.transform.localScale.z);
+        }
+        else if (this.transform.localScale.y < lenght)
+        {
+            this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y + force, this.transform.localScale.z);
+        }
+
+    }
+
+    public void setForce(float newForce)
+    {
+        this.force = newForce;
+    }
+
+    public float getForce()
+    {
+        return this.force;
+    }
+}
